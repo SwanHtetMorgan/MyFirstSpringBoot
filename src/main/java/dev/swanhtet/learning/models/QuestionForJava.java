0579a6fc052f82c8fd3cf4018dadee7d54cb140f
Maven <swanhtet102002@gmail.com>
2024-04-10 22:43:55 +0200

    My First Spring Boot project API

---
+++ src/main/java/dev/swanhtet/learning/models/QuestionForJava.java
@@ -0,0 +1,34 @@
+package dev.swanhtet.learning.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class QuestionForJava {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private  String Question;
+    private String Option1;
+    private String Option2;
+    private String Option3;
+    private String rightAns;
+    private String category;
+
+    public QuestionForJava(String question, String option1, String option2, String option3, String rightAns, String category) {
+        Question = question;
+        Option1 = option1;
+        Option2 = option2;
+        Option3 = option3;
+        this.rightAns = rightAns;
+        this.category = category;
+    }
+
+    // DONE For the Hibernate
+    public QuestionForJava() {
+
+    }
+}
+
